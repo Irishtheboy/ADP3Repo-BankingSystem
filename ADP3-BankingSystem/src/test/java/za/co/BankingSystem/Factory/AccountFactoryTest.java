35fe4e13628bfe34a9abfca9fdaa218cfe4a2056
LeGoober <rorisangmakganaiv@gmail.com>
2025-03-28 21:11:29 +0200

    Finalized testing

--- ADP3-BankingSystem/src/test/java/za/co/BankingSystem/Factory/AccountFactoryTest.java
+++ ADP3-BankingSystem/src/test/java/za/co/BankingSystem/Factory/AccountFactoryTest.java
@@ -1,7 +1,38 @@
+/*
+ * AccountFactoryTest.java
+ * AccountFactoryTest Class
+ * Author: Rorisang Makgana (230602363)
+ * Date: 28th March 2025
+ */
 package za.co.BankingSystem.Factory;
+
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.ClassOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(OrderAnnotation.class)
 class AccountFactoryTest {
+    AccountFactory accountFactory1 = new AccountFactory();
+    AccountFactory accountFactory2 = new AccountFactory();
+    AccountFactory accountFactory3 = new AccountFactory();
 
+    @Test
+    @Order(1)
+    public void testCreateAccount() {
+        assertNotNull(accountFactory1);
+    }
+    @Test
+    @Order(2)
+    public void testAccountWithAllParams() {
+        assertNotNull(accountFactory2);
+    }
+    @Test
+    @Order(3)
+    public void testFailAccount() {
+        //fail(accountFactory3);
+    }
 }
